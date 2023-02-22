d9ef3667c8130c61bffb596bde3151f1dd0a370d
StevenJBurns <StevenJBurns@users.noreply.github.com>
2023-02-21 22:20:23 -0700

    chore: formatting clean up

--- Application.cs
+++ Application.cs
@@ -1,22 +1,18 @@
-﻿using System;
+using System;
 using System.Windows;
 using Science.Chemistry;
 
 namespace Science
-	{
-	class App : Application
-		{
-		public App()
-			{
-			}
-		
-		[STAThread]
-		static void Main()
-			{
-			App app = new App();
-			app.Run(new WindowPeriodicTable());
-			}
-		
-		}
-		
-	}
+{
+  class App : Application
+  {
+    public App() {}
+    
+    [STAThread]
+    static void Main()
+    {
+      App app = new App();
+      app.Run(new WindowPeriodicTable());
+    }
+  }
+}
