6336e8e8cb8095d77fec38632f7079a1a4488af7
StevenJBurns <StevenJBurns@users.noreply.github.com>
2024-04-03 07:14:29 -0500

    chore: clean up formatting in Element.cs

--- Element.cs
+++ Element.cs
@@ -2,32 +2,25 @@
 using System.Collections.Generic;
 
 namespace Science.Chemistry
-	{
-	class AtomicElement
-		{
-		public Byte AtomicNumber
-			{ get; set; }
-		
-		public String Symbol
-			{ get; set; }
-		
-		public String Name
-			{ get; set; }
-			
-		public Byte Group
-			{ get; set; }
+{
+  class AtomicElement
+  {
+    public Byte AtomicNumber { get; set; }
 
-		public Byte Period
-			{ get; set; }
-		
-		public Double AtomicWeight
-			{ get; set; }
+    public String Symbol { get; set; }
 
-		public Double AtomicRadius
-			{ get; set; }
-		}
-	
-	class AtomicElements : List<AtomicElement>
-		{
-		}
-	}
+    public String Name { get; set; }
+
+    public Byte Group { get; set; }
+
+    public Byte Period { get; set; }
+
+    public Double AtomicWeight { get; set; }
+
+    public Double AtomicRadius { get; set; }
+  }
+
+  class AtomicElements : List<AtomicElement>
+  {
+  }
+}
