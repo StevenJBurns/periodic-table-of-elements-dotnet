41e267354198d3a7f77e64d18e605a55a9e25596
StevenJBurns <StevenJBurns@users.noreply.github.com>
2025-07-10 14:11:33 -0500

    feat: add AtomicElementRepository from very old OneDrive source

---
+++ Domain/AtomicElementRepository.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace SJB.Science.PeriodicTable.Model;
+
+public class AtomicElementRepository : ObservableCollection<AtomicElement>
+  {
+		
+  }
