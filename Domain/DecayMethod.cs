2f0e6f61095dd9d99648674745b47871772c4aa9
StevenJBurns <StevenJBurns@users.noreply.github.com>
2025-07-10 14:19:09 -0500

    feat: add DecayMethod enum from very old OneDrive source code

---
+++ Domain/DecayMethod.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace SJB.Science.PeriodicTable.Model;
+
+public enum DecayType
+  {
+  Unknown = 0,
+  Stable,
+  AlphaEmission,
+  ProtonEmission,
+  TwoProtonEmission,
+  NeutronEmission,
+  TwoNeutronEmission,
+  BetaDecay,
+  DoubleBetaDecay,
+  BetaPlusDecay,
+  DoubleBetaPlusDecay,
+  ElectronCapture,
+  SpontaneousFission
+  }
