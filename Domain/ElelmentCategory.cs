35b9d6a0929ca487681203db69a6cb90d7d2a4b4
StevenJBurns <StevenJBurns@users.noreply.github.com>
2025-07-10 15:07:02 -0500

    feat: add ElelmentCategory enum from very old OneDrive source code

---
+++ Domain/ElelmentCategory.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace SJB.Science.PeriodicTableOfElements.Domain;
+
+public enum ElementCategory
+  {
+  Unknown = 0,
+  AlkaliMetals,
+  AlkalineEarthMetals,
+  Lanthanides,
+  Actinides,
+  TransitionElements,
+  OtherMetals,
+  Metalloids,
+  OtherNonMetals,
+  Halogens,
+  NobleGases
+  }
+  
