c72de1cdcdd4d4b445545df328386e6259a4abb3
SJB <stevenjburns@msn.com>
2025-07-11 06:46:14 -0500

    feat: add WindowMain from very old OneDrive source code

---
+++ src/Presentation.WPF/Windows/WindowMain/WindowMain.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Windows;
+
+namespace PeriodicTableOfElements.Presentation.WPF.Windows;
+
+public partial class WindowMain : Window
+{
+  public WindowMain()
+  {
+    InitializeComponent();
+  }
+}
