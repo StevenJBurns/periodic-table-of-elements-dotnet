3733817d878af9b6eff948cf7dfaa6d423c1a2db
SJB <stevenjburns@msn.com>
2025-07-11 05:57:35 -0500

    feat: add TableElement control from very old OneDrive source code

---
+++ src/Presentation.WPF/Controls/TableElement/TableElement.cs
@@ -0,0 +1,14 @@
+using System.Windows.Controls;
+
+namespace PeriodicTableOfElements.Presentation.WPF;
+
+/// <summary>
+/// Interaction logic for TableElement.xaml
+/// </summary>
+public partial class PeriodicTableElement : UserControl
+{
+  public PeriodicTableElement()
+  {
+    InitializeComponent();
+  }
+}
